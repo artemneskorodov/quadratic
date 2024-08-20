fbd017acd70a5e656441dad174714d1617e61f9a
Chocolate <artemnes1j@gmail.com>
2024-08-20 16:05:13 +0300

    printig out function added

--- quadratic.c
+++ quadratic.c
@@ -10,6 +10,7 @@ const int UNEXPECTED_ERROR_WHILE_SOLVING_QUADRATIC = -3;
 
 int solve_quadratic(double a, double b, double c, double *x1, double *x2);
 int compare_doubles(double a, double b);
+void print_quadratic_roots(int number, double x1, double x2);
 
 
 
@@ -27,37 +28,8 @@ int main(void){
     int number = solve_quadratic(a, b, c, &x1, &x2);
 
     //Printing answers in console
-    switch(number){
-        //2 roots
-        case 2: {
-            printf("x1 = %f, x2 = %f\n", x1, x2);
-            break;
-        }
-        //1 root
-        case 1: {
-            printf("x = %f", x1);
-            break;
-        }
-        //no roots
-        case 0: {
-            printf("No real roots\n");
-            break;
-        }
-        //invalid coeffitients(a == 0)
-        case INVALID_COEFFITIENTS_FOR_QUADRATIC: {
-            printf("Invalid coeffitients\n");
-            break;
-        }
-        //infinitly many roots(a == b == c == 0)
-        case INFINITLY_MANY_ROOTS: {
-            printf("Every real number\n");
-            break;
-        }
-        default: {
-            printf("Error\n");
-            return 1;
-        }
-    }
+    print_quadratic_roots(number, x1, x2);
+
     return 0;
 }
 
@@ -117,4 +89,40 @@ int compare_doubles(double a, double b){
     if(fabs(a - b) < EPSILON) return 0;
     if(a < b) return -1;
     return 1;
+}
+
+
+
+//prints roots of quadratic equation in console
+void print_quadratic_roots(int number, double x1, double x2){
+    switch(number){
+        //2 roots
+        case 2: {
+            printf("x1 = %f, x2 = %f\n", x1, x2);
+            break;
+        }
+        //1 root
+        case 1: {
+            printf("x = %f", x1);
+            break;
+        }
+        //no roots
+        case 0: {
+            printf("No real roots\n");
+            break;
+        }
+        //invalid coeffitients(a == 0)
+        case INVALID_COEFFITIENTS_FOR_QUADRATIC: {
+            printf("Invalid coeffitients\n");
+            break;
+        }
+        //infinitly many roots(a == b == c == 0)
+        case INFINITLY_MANY_ROOTS: {
+            printf("Every real number\n");
+            break;
+        }
+        default: {
+            printf("Error\n");
+        }
+    }
 }
